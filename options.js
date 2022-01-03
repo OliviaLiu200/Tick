d7f666e1de0008af9d250754770a1fe8d2a2d5f4
StephenHuang3 <91025265+StephenHuang3@users.noreply.github.com>
2022-01-03 00:06:26 -0500

    renew options page

--- options.js
+++ options.js
@@ -1,17 +1,9 @@
-'use strict';
+window.onload = function() {
+    document.getElementById('save').onclick = function(){
+    var value = document.getElementById('save new name').value;
+    alert("your name is now"+ value);
+    //alert(value);
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-
-function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-        let vutton = document.createElement(`button`);
-        button.addEventListener(`click`, function() {
-            chrome.storage.sync.set({color: item}, function() {
-                console.log(`color is ` + item);
-            });
-        });
-        page.appendChild(button);
     }
+
 }
-constructOptions(kButtonColors);
