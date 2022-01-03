fae9baf827507284170e5febe568124d6ea28ce4
StephenHuang3 <91025265+StephenHuang3@users.noreply.github.com>
2022-01-03 14:53:03 -0500

    added change hour limit function to options page

--- options.js
+++ options.js
@@ -5,7 +5,35 @@ window.onload = function() {
         alert("your name is now "+ value);
     });
 
-
     }
+
+    document.getElementById('hourlimit').onclick = function(){
+        var hourlimit = document.getElementById('save new hour limit').value; //need to make sure input is a number
+        if (checkInp(hourlimit)) {
+            alert("please input a number");
+
+        } else if ((hourlimit <= 0) || (hourlimit >=24)){
+            alert("please input a valid number between 1 and 23");
+        }
+        
+        else {
+        chrome.storage.sync.set({'name': hourlimit}, function(){
+            alert("your new time limit is now "+ hourlimit + " hours");
+        });
+        }
+    
+        }
+
+        function checkInp(valueToCheck) {
+    
+        var regex=/^[0-9]+$/;
+        if (valueToCheck.match(regex))
+        {
+       
+        return false;
+        }
+        else return true;
+}
+    
 
 }
