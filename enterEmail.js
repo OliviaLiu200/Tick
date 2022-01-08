93ba5a47186d49632d15bdad8f4a569df750fd8e
StephenHuang3 <91025265+StephenHuang3@users.noreply.github.com>
2022-01-07 22:11:00 -0500

    make enteremail.js easier to read

--- enterEmail.js
+++ enterEmail.js
@@ -1,40 +1,26 @@
 window.onload = function() {
 
 form.addEventListener('submit', (e) => {
-    var fname = document.getElementById('name');
+var fname = document.getElementById('name');
 var friendemail = document.getElementById('email');
+
 //chrome.storage.local.set({'storedinfname': fname})
 
-let messages = []
+ chrome.storage.local.get(['flist', 'femail'], function(result){
+     let storedfriendlist = result['flist']
+     let storedfriendemail = result['femail']
 
-if (fname.value === '' || fname.value == null){
-    messages.push('Name is required')
-}
+ storedfriendlist.push(fname)
+ storedfriendemail.push(friendemail)
 
-if (friendemail.value ==='' || friendemail.value == null){
-    messages.push('Email is required')
-}
-
-if (messages.length > 0) {
-    errorElement.innerText = messages.join (', ')
-    e.preventDefault()
-} else {
-
-        chrome.storage.local.get(['flist', 'femail'], function(result){
-            let storedfriendlist = result['flist']
-            let storedfriendemail = result['femail']
-
-        storedfriendlist.push(fname)
-        storedfriendemail.push(friendemail)
-
-        chrome.storage.local.set({'flist': storedfriendlist})
-        chrome.storage.local.set({'femail': storedfriendemail})
+    chrome.storage.local.set({'flist': storedfriendlist})
+    chrome.storage.local.set({'femail': storedfriendemail})
         
-        window.location = 'friendsListYeet.html'
+    window.location = 'friendsListYeet.html'
     });
     
   
-}
+
 })
 
 }
